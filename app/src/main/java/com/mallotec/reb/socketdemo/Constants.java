bab37a722031dbbdb9a33442705b5785863d4298
Reborn <ren_xiaoyao@outlook.com>
2019-05-20 21:19:29 +0800

    获得独一无二的Psuedo ID来区分不同设备

--- app/src/main/java/com/mallotec/reb/socketdemo/Constants.java
+++ app/src/main/java/com/mallotec/reb/socketdemo/Constants.java
@@ -1,8 +1,10 @@
 package com.mallotec.reb.socketdemo;
 
+import com.mallotec.reb.socketdemo.tool.DeviceUtil;
+
 /**
  * Created by reborn on 2019-05-20.
  */
 public class Constants {
-    public static final String ID = "Reborn";
+    public static final String ID = DeviceUtil.getUniquePsuedoID();
 }
